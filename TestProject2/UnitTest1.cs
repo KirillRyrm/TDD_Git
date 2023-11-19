13a41dc7d08c0689bafb492db4ac79e57342ec16
KirillRyrm <rirmakkirill@gmail.com>
2023-11-19 22:28:17 +0200

    Added test TestFrancMultiplication

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -11,5 +11,13 @@ namespace TestProject2
             Assert.AreEqual(Money.dollar(10), five.Times(2));
             Assert.AreEqual(Money.dollar(15), five.Times(3));
         }
+
+        [Test]
+        public void TestFrancMultiplication()
+        {
+            Money five = Money.franc(5);
+            Assert.AreEqual(Money.franc(10), five.Times(2));
+            Assert.AreEqual(Money.franc(15), five.Times(3));
+        }
     }
 }
