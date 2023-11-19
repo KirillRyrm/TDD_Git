618a0b75822ef20f7a4ce35ff2379399f17c4e78
KirillRyrm <rirmakkirill@gmail.com>
2023-11-19 22:21:15 +0200

    Currencies franc and dollar added

---
+++ TestProject2/Dollar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    public class Dollar : Money
+    {
+        public Dollar(int amount, string currency) : base(amount, currency) { }
+
+
+        public override Money Times(int multiplier)
+        {
+            return new Money(amount * multiplier, currency);
+        }
+
+        static Money dollar(int amount)
+        {
+            return new Money(amount, "USD");
+        }
+
+    }
+}
---
+++ TestProject2/Franc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    public class Franc : Money
+    {
+        public Franc(int amount, string currency) : base(amount, currency) { }
+
+
+        public override Money Times(int multiplier)
+        {
+            return new Money(amount * multiplier, currency);
+        }
+
+        static Money franc(int amount)
+        {
+            return new Money(amount, "CHF");
+        }
+
+
+    }
+}
