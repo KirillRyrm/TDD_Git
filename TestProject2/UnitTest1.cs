ec35c255f5a1a41d6e2def0eb90a5a9d567e758c
KirillRyrm <rirmakkirill@gmail.com>
2023-11-19 22:30:52 +0200

    Added test testEquality for equal objects of currencies

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -19,5 +19,20 @@ namespace TestProject2
             Assert.AreEqual(Money.franc(10), five.Times(2));
             Assert.AreEqual(Money.franc(15), five.Times(3));
         }
+
+        [Test]
+        public void testEquality()
+        {
+            Assert.True(Money.dollar(5).Equals(Money.dollar(5)));
+            Assert.False(Money.dollar(5).Equals(Money.dollar(6)));
+            Assert.True(Money.franc(5).Equals(Money.franc(5)));
+            Assert.False(Money.franc(5).Equals(Money.franc(6)));
+            Assert.True(Money.dollar(5).Equals(Money.dollar(5)));
+            Assert.True(Money.dollar(5).Equals(Money.dollar(5)));
+            Assert.True(Money.franc(5).Equals(Money.franc(5)));
+            Assert.False(Money.franc(5).Equals(Money.franc(6)));
+            Assert.False(Money.franc(5).Equals(Money.dollar(5)));
+
+        }
     }
 }
